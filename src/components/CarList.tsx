c9dd76fa2e4790198e8ba22d82c8588681d8f3f5
juhahinkula <hinkula@gmail.com>
2026-03-24 10:33:37 +0200

    Add functionality added

--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
-import type { CarData } from "../types";
-import type { GridColDef } from '@mui/x-data-grid';
+import type { CarData, Car } from "../types";
+import type { GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
 import { DataGrid } from "@mui/x-data-grid";
+import Button from "@mui/material/Button";
+import AddCar from "./AddCar";
+import Stack from "@mui/material/Stack";
 
 function CarList() {
   const [cars, setCars] = useState<CarData[]>([]);
@@ -12,7 +15,20 @@ function CarList() {
     { field: "color", headerName: "Color", width: 100 },
     { field: "fuel", headerName: "Fuel" },
     { field: "modelYear", headerName: "Model year" },
-    { field: "price", headerName: "Price (€)" },        
+    { field: "price", headerName: "Price (€)" },
+    {
+      field: "_links.self.href",
+      headerName: "",
+      sortable: false,
+      filterable: false,
+      renderCell: (params: GridRenderCellParams) =>
+        <Button 
+          color="error" 
+          size="small" 
+          onClick={() => handleDelete(params.id as string)}>
+          Delete
+        </Button>
+    },        
   ]
 
   const getCars = () => {
@@ -27,18 +43,56 @@ function CarList() {
     .catch(err => console.error(err))
   } 
 
+  const handleDelete = (url: string) => {
+    if (window.confirm("Are you sure?")) {
+      fetch(url, {
+        method: "DELETE"
+      })
+      .then(response => {
+        if (!response.ok)
+          throw new Error("Error when deleting a car");
+
+        return response.json();
+      })
+      .then(() => getCars())
+      .catch(err => console.error(err));
+    }
+  }
+
+  const handleAdd = (car: Car) => {
+    fetch(import.meta.env.VITE_API_URL + "/cars", {
+      method: "POST",
+      headers: {
+        "Content-type":"application/json"
+      },
+      body: JSON.stringify(car)
+    })
+    .then(response => {
+      if (!response.ok)
+        throw new Error("Error when adding a car");
+
+      return response.json();
+    })
+    .then(() => getCars())
+    .catch(err => console.error(err));
+  }
+
   useEffect(() => {
     getCars();
   }, []);
 
   return(
     <>
+      <Stack direction="row" sx={{ mt: 2, mb: 2 }} >
+        <AddCar handleAdd={handleAdd} />
+      </Stack>
       <div style={{ width: "90%", height: 500 }}>
         <DataGrid 
           rows={cars}
           columns={columns}
           getRowId={row => row._links.self.href}
           autoPageSize
+          rowSelection={false}
         />
       </div>
     </>
